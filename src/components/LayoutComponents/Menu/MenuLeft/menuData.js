bb1de43aaa09d36f1812d917907802129f4798c4
David Barnes <dbarnes@ncsoffice.local>
2019-01-09 15:14:05 -0600

    first commit oh yeah

---
+++ src/components/LayoutComponents/Menu/MenuLeft/menuData.js
@@ -0,0 +1,188 @@
+export default [
+    {
+        title: 'Home',
+        key: 'dashboardAlpha',
+        url: '/dashboard/alpha',
+        icon: 'icmn icmn-stack',
+        children: [
+            {
+                title: 'Dashboard',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Admin Dashboard',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Dashboard Settings',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            }
+        ]
+    },
+    {
+        title: 'Time Clock',
+        key: 'timeclock',
+        url: '/timeclock',
+        icon: 'icmn icmn-books',
+    },
+    {
+        title: 'Calendar',
+        key: 'calendar',
+        url: '/calendar',
+        icon: 'icmn icmn-books',
+        children: [
+            {
+                title: 'View Calendar',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Submit PTO Request',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Add Calendar Event',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            }
+        ]
+    },
+    {
+        title: 'Policies & Procedures',
+        key: 'documents',
+        url: '/documents',
+        icon: 'icmn icmn-books'
+    },
+    {
+        title: 'My User Settings',
+        key: 'usersettings',
+        url: '/userSettings',
+        icon: 'icmn icmn-books',
+        children: [
+            {
+                title: 'About Me',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Update Password',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Update Security Questions',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            }
+        ]
+    },
+    {
+        title: 'Administration',
+        key: 'admin',
+        url: '/admin',
+        icon: 'icmn icmn-books',
+    },
+    {
+        title: 'Reports',
+        key: 'reports',
+        url: '/reports',
+        icon: 'icmn icmn-books',
+        children: [
+            {
+                title: 'PBJ Reporting',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Timesheet Reporting',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Future - Custom Reports',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            }
+        ]
+    },
+    {
+        title: 'System Settings',
+        key: 'systemSettings',
+        url: '/systemSettings',
+        icon: 'icmn icmn-books',
+        children: [
+            {
+                title: 'User',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Group',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Vehicle',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Location',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Onboarding',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Global System Settings',
+                key: 'empty',
+                url: '/empty',
+                icon: 'icmn icmn-books',
+            }
+        ]
+    },
+    {
+        title: 'Developer Samples',
+        key: 'developer',
+        url: '/developer',
+        icon: 'icmn icmn-books',
+        children: [
+            {
+                title: 'Dashboard Sample',
+                key: 'sampleDashboard',
+                url: '/dashboard/alpha',
+                icon: 'icmn icmn-books',
+            },
+            {
+                title: 'Simple Page Sample',
+                key: 'simplePage',
+                url: '/antdesign/affix',
+                icon: 'icmn icmn-books',
+            }
+        ]
+    },
+]
